8889ff8bd2933ae8d929b9d44b24f5685c09daa2
Mire96 <milos.m96@hotmail.com>
2017-03-22 19:25:24 +0100

    Napravljena klasa Valuta sa odgovarajucim privatnim atributima

---
+++ menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,12 @@
+package menjacnica;
+
+import java.sql.Date;
+
+public class Valuta {
+	private String naziv;
+	private String skraceni;
+	private Date datum;
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+}
