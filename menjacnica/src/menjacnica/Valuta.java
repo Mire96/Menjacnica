a1d4fe7d3a65413909acda3f39bf9109ff56a172
Mire96 <milos.m96@hotmail.com>
2017-03-22 19:30:21 +0100

    Dodate get i set metode, koje sam zaboravio u prethodnom commitu

--- menjacnica/src/menjacnica/Valuta.java
+++ menjacnica/src/menjacnica/Valuta.java
@@ -9,4 +9,42 @@ public class Valuta {
 	private double prodajni;
 	private double kupovni;
 	private double srednji;
+	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceni() {
+		return skraceni;
+	}
+	public void setSkraceni(String skraceni) {
+		this.skraceni = skraceni;
+	}
+	public Date getDatum() {
+		return datum;
+	}
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
 }
