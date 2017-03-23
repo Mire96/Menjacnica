23c3bd6440768d69b7e30804a77a61eeeceadfc2
Mire96 <milos.m96@hotmail.com>
2017-03-23 18:57:07 +0100

    Uvedena klasa KursnaLista koja implementira InterfejsMenjacnice

---
+++ menjacnica/src/menjacnica/KursnaLista.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.sql.Date;
+
+import interfejsMenjacnice.InterfejsMenjacnice;
+
+public class KursnaLista implements InterfejsMenjacnice {
+
+	@Override
+	public void dodavanjeKursaZaDan(double prodajni, double kupovni, double srednji, Date datum) {
+		
+
+	}
+
+	@Override
+	public void brisanjeKursaZaDan(Date datum) {
+		
+
+	}
+
+	@Override
+	public Valuta pronadjiKursZaDan(Date datum) {
+		
+		return null;
+	}
+
+}
