6b6f09fae5a6edbfb3313f257027a68f120bcae8
Mire96 <milos.m96@hotmail.com>
2017-03-23 19:00:14 +0100

    Dodat privatni atribut lista u klasu KursnaLista

--- menjacnica/src/menjacnica/KursnaLista.java
+++ menjacnica/src/menjacnica/KursnaLista.java
@@ -1,11 +1,14 @@
 package menjacnica;
 
 import java.sql.Date;
+import java.util.LinkedList;
 
 import interfejsMenjacnice.InterfejsMenjacnice;
 
 public class KursnaLista implements InterfejsMenjacnice {
-
+	
+	private LinkedList<Valuta> lista;
+    
 	@Override
 	public void dodavanjeKursaZaDan(double prodajni, double kupovni, double srednji, Date datum) {
 		
