5d2d05f2948cf9ae63c00f5f9b7f44548bfe97ab
Mire96 <milos.m96@hotmail.com>
2017-03-23 18:34:17 +0100

    Dodate u klasi Valuta metode equals, hashCode i toString

--- menjacnica/src/menjacnica/Valuta.java
+++ menjacnica/src/menjacnica/Valuta.java
@@ -11,6 +11,61 @@ public class Valuta {
 	private double srednji;
 	
 	
+	
+	@Override
+	public String toString() {
+		return "Valuta " + naziv + ", skraceni naziv " + skraceni + ", na datum" + datum + " ima prodajni kurs: " + prodajni
+				+ ", kupovni kurs: " + kupovni + "i srednji kurs: " + srednji;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceni == null) ? 0 : skraceni.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skraceni == null) {
+			if (other.skraceni != null)
+				return false;
+		} else if (!skraceni.equals(other.skraceni))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	
 	public String getNaziv() {
 		return naziv;
 	}
@@ -47,4 +102,5 @@ public class Valuta {
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
 	}
+	
 }
